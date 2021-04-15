faee28bc9eb4f40826252ffdb22fcd8ef24f89fe
Dan Armour <RandomComm3nt@users.noreply.github.com>
2021-04-15 12:34:54 +0100

    Added ActivationText

---
+++ Scripts/Model/Cards/Modules/ActivationText.cs
@@ -0,0 +1,28 @@
+﻿﻿using CcgCore.Model.Effects;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CcgCore.Model.Cards.Modules
+{
+    public class ActivationText : CardDefinitionModule
+    {
+        [SerializeField, FoldoutGroup("Activation Text"), HideReferenceObjectPicker] private List<TextOption> context = new List<TextOption>();
+        [SerializeField, FoldoutGroup("Activation Text"), HideReferenceObjectPicker] private List<ActionTextOption> action = new List<ActionTextOption>();
+        [SerializeField, FoldoutGroup("Activation Text"), HideReferenceObjectPicker] private List<TextOption> flavour = new List<TextOption>();
+
+        private class TextOption
+        {
+            [field: SerializeField, FoldoutGroup("@DisplayLabel")] public List<TriggerCondition> Conditions { get; private set; } = new List<TriggerCondition>();
+            [field: SerializeField, FoldoutGroup("@DisplayLabel")] public string Text { get; private set; }
+            [field: SerializeField, FoldoutGroup("@DisplayLabel")] public float Priority { get; private set; }
+
+            private string DisplayLabel => Text.Substring(0, 100) + (Text.Length > 100 ? "..." : "");
+        }
+
+        private class ActionTextOption
+        {
+            [field: SerializeField, FoldoutGroup("@DisplayLabel")] public bool AddFlavourText { get; private set; }
+        }
+    }
+}
