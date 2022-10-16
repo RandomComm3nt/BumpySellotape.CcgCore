c0f04488f59aa1fed156c0d1fd802a54e9c1b7a9
Dan <RandomComm3nt@users.noreply.github.com>
2022-10-16 19:30:15 +0100

    Added EndGameEffect

---
+++ Scripts/Model/Effects/EndGameEffect.cs
@@ -0,0 +1,24 @@
+﻿using BumpySellotape.Events.Model.Effects;
+using CcgCore.Model;
+using CcgCore.Model.Effects;
+using CcgCore.Model.Parameters;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BumpySellotape.CcgCore.CcgCore.Model.Effects
+{
+    public class EndGameEffect : IEffect
+    {
+        [SerializeField] private bool endImmediately;
+
+        public string Label => "End Game";
+
+        public void Process(ProcessingContext processingContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
